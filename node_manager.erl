61f31feb166ca5ac4794a68d7eeb02b60c601947
Kohei Takahashi <flast@flast.jp>
2011-11-27 01:56:35 +0900

    refactor

--- node_manager.erl
+++ node_manager.erl
@@ -1,6 +1,5 @@
 -module(node_manager).
 -export([get/0, new/1, new_link/1, enter/1, leave/1]).
--import(lists, [reverse/1]).
 
 get() ->
   try
@@ -22,33 +21,39 @@ new(Nodes, Spawn) when is_list(Nodes) ->
 new(Nodes) -> new(Nodes, fun spawn/1).
 new_link(Nodes) -> new(Nodes, fun spawn_link/1).
 
-nodes_RR([]) ->
-  receive
-    {Pid, get} ->
-      Pid ! {clusterccd_nodes_pool, self(), undefined},
-      Next = [];
+nodes_get(Pid, [H | T]) ->
+  Pid ! {clusterccd_nodes_pool, self(), H},
+  lists:reverse([H | lists:reverse(T)]);
+nodes_get(Pid, []) ->
+  Pid ! {clusterccd_nodes_pool, self(), undefined},
+  [].
 
-    {manage, enter, Node} ->
-      Next = [Node],
-      common_io:prefixed("enter new node: ~w", Next);
-    {manage, leave, _} ->
-      common_io:prefixed("cannot leave node"),
-      Next = []
-  end,
-  nodes_RR(Next);
-
-nodes_RR(L = [H | T]) ->
-  receive
-    {Pid, get} ->
-      Pid ! {clusterccd_nodes_pool, self(), H},
-      Next = reverse([H | reverse(T)]);
-
-    {manage, enter, Node} ->
+nodes_enter(Node, L) ->
+  case lists:member(Node, L) of
+    true ->
+      common_io:prefixed("such node already entered"),
+      L;
+    false ->
       common_io:prefixed("enter new node: ~w", [Node]),
-      Next = [Node | L];
-    {manage, leave, Node} ->
+      [Node | L]
+  end.
+
+nodes_leave(Node, L) ->
+  case lists:member(Node, L) of
+    true ->
       common_io:prefixed("leave node: ~w", [Node]),
-      Next = lists:delete(Node, L)
+      lists:delete(Node, L);
+    false ->
+      common_io:prefixed("cannot leave such node"),
+      L
+  end.
+
+nodes_RR(L) when is_list(L) ->
+  Next = receive
+    {Pid, get} -> nodes_get(Pid, L);
+
+    {manage, enter, Node} -> nodes_enter(Node, L);
+    {manage, leave, Node} -> nodes_leave(Node, L)
   end,
   nodes_RR(Next).
 
